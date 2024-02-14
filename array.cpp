9d18179b6d47d65f621250acb03e09595c076c1d
Tharun-Raj-Kuntluru <158204723+Tharun-Raj-Kuntluru@users.noreply.github.com>
2024-02-14 10:03:46 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array[10];
+    array[0] = 100;
+    cout << array[0] <<endl;
+    // cout << array[5] <<endl;            //Garbage value
+    // cout << array[100] <<endl;          //Garbage value
+}
